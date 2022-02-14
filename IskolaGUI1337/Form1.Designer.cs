6a5182ad5a11865d3fba7a8b02356fd735a3de5a
gyeneics12d <gyeneics.12d@acsjszki.hu>
2022-02-14 13:46:29 +0100

    Másik eljárással 1337. feladat

---
+++ IskolaGUI1337/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace IskolaGUI1337
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTorles = new System.Windows.Forms.Button();
+            this.btnMentes = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnBetoltes = new System.Windows.Forms.Button();
+            this.btnKilepes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTorles
+            // 
+            this.btnTorles.Location = new System.Drawing.Point(10, 372);
+            this.btnTorles.Name = "btnTorles";
+            this.btnTorles.Size = new System.Drawing.Size(141, 30);
+            this.btnTorles.TabIndex = 0;
+            this.btnTorles.Text = "Törlés";
+            this.btnTorles.UseVisualStyleBackColor = true;
+            // 
+            // btnMentes
+            // 
+            this.btnMentes.Location = new System.Drawing.Point(157, 372);
+            this.btnMentes.Name = "btnMentes";
+            this.btnMentes.Size = new System.Drawing.Size(141, 30);
+            this.btnMentes.TabIndex = 1;
+            this.btnMentes.Text = "Mentés";
+            this.btnMentes.UseVisualStyleBackColor = true;
+            this.btnMentes.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(288, 342);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btnBetoltes
+            // 
+            this.btnBetoltes.Location = new System.Drawing.Point(10, 408);
+            this.btnBetoltes.Name = "btnBetoltes";
+            this.btnBetoltes.Size = new System.Drawing.Size(141, 30);
+            this.btnBetoltes.TabIndex = 3;
+            this.btnBetoltes.Text = "Betöltés";
+            this.btnBetoltes.UseVisualStyleBackColor = true;
+            // 
+            // btnKilepes
+            // 
+            this.btnKilepes.Location = new System.Drawing.Point(157, 408);
+            this.btnKilepes.Name = "btnKilepes";
+            this.btnKilepes.Size = new System.Drawing.Size(141, 29);
+            this.btnKilepes.TabIndex = 4;
+            this.btnKilepes.Text = "Kilépés (exit)";
+            this.btnKilepes.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 450);
+            this.Controls.Add(this.btnKilepes);
+            this.Controls.Add(this.btnBetoltes);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnMentes);
+            this.Controls.Add(this.btnTorles);
+            this.Name = "Form1";
+            this.Text = "IskolaGUI1337";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTorles;
+        private System.Windows.Forms.Button btnMentes;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnBetoltes;
+        private System.Windows.Forms.Button btnKilepes;
+    }
+}
+
